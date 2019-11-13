313a335f60d3f5979dda8f33437460a9c5d45c53
Glorfischi <Glorfischi@users.noreply.github.com>
2019-11-13 14:51:37 +0100

    Added quick dirty start script for IdP

---
+++ hack/start_idp.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+docker rm idp-mysql
+docker run --name idp-mysql -p 3306:3306 -v $(pwd)/../ansible/roles/mysql/files:/docker-entrypoint-initdb.d \
+-e MYSQL_ROOT_PASSWORD=foo  -e MYSQL_DATABASE=imovies -e MYSQL_USER=user -e MYSQL_PASSWORD=pass mysql
+
+cd ./../IdP/
+go run . -dsn="user:pass@(localhost)/imovies"
