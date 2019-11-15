3ad874e0a8e972e89e3b153b01adcbfab9275b34
David Bimmler <git@d4ve.email>
2019-11-15 10:52:13 +0100

    create pki user

--- IdP/vault.go
+++ IdP/vault.go
@@ -4,7 +4,10 @@ import (
 	"fmt"
 	"github.com/hashicorp/vault/api"
 	log "github.com/sirupsen/logrus"
+	"regexp"
 )
+
+const alphanumeric = "[[:alnum:]]"
 
 type vault struct {
 	c *api.Logical
@@ -27,19 +30,45 @@ func NewVaultClient(vaultAddress string) (*vault, error) {
 }
 
 func (v *vault) PKIRoleExists(name string) (bool, error) {
-	// TODO(bimmlerd) this might be a vuln, have not thought about it much
-	s, err := v.c.Read(fmt.Sprintf("/pki/roles/%s", name))
+	l := log.WithField("name", name)
+	if !regexp.MustCompile(alphanumeric).MatchString(name) {
+		l.Error("Invalid name format.")
+		return false, fmt.Errorf("invalid name format")
+	}
+	role, err := v.c.Read(fmt.Sprintf("/pki/roles/%s", name))
 	if err != nil {
-		log.WithError(err).WithField("name", name).Error("Failed to fetch PKI role.")
+		l.WithError(err).WithField("name", name).Error("Failed to fetch PKI role.")
 		return false, err
 	}
-	if s == nil {
-		log.WithField("name", name).Debug("No PKI Role with this name.")
+	if role == nil {
+		l.WithField("name", name).Debug("No PKI Role with this name.")
 		return false, nil
 	}
 	return true, nil
 }
 
 func (v *vault) CreatePKIUser(name string) error {
+	l := log.WithField("name", name)
+	if !regexp.MustCompile(alphanumeric).MatchString(name) {
+		l.Error("Invalid name format.")
+		return fmt.Errorf("invalid name format")
+	}
+	// www.vaulptproject.io/api/secret/pki/index.html#create-update-role
+	// Note that we depend on a lot of secure defaults here, such as key type and length.
+	_, err := v.c.Write(fmt.Sprintf("/pki/roles/%s", name), map[string]interface{}{
+		"allow_localhost": false,
+		"allowed_domains": []string{fmt.Sprintf("%s@fadalax.tech", name)},
+		"enforce_hostnames": true,
+		"allow_ip_sans": false,
+		"server_flag": false, // Should not be used as server certs.
+		"client_flag": true, // Since we are want to generate certs which can be used for auth.
+		"email_protection_flag": true, // Emails are the other core purpose.
+		"organization": "imovies",
+		"country": "CH",
+	})
+	if err != nil {
+		l.WithError(err).Error("Failed to create PKI role.")
+		return err
+	}
 	return fmt.Errorf("not implemented")
 }
